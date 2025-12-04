8d6ec63cf0e4e8503c2e54073e7351822728bc0e
Kyomxra <tiffanypatricianova@gmail.com>
2025-12-04 22:27:50 +0700

    Commit No. 1 – add data binding

--- app/src/main/java/com/example/lab_week_13/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_13/MainActivity.kt
@@ -1,18 +1,18 @@
-package com.example.test_lab_week_12
+package com.example.lab_week_13
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.RecyclerView
-import com.example.test_lab_week_12.model.Movie
+import com.example.lab_week_13.databinding.ActivityMainBinding
+import com.example.lab_week_13.model.Movie
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
-import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -25,10 +25,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.movie_list)
-        recyclerView.adapter = movieAdapter
+        val binding: ActivityMainBinding = DataBindingUtil
+            .setContentView(this, R.layout.activity_main)
+
+        binding.movieList.adapter = movieAdapter
 
         val movieRepository = (application as MovieApplication).movieRepository
 
@@ -42,34 +43,11 @@ class MainActivity : AppCompatActivity() {
             }
         )[MovieViewModel::class.java]
 
-        // Collect flows safely using repeatOnLifecycle
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+        binding.viewModel = movieViewModel
+        binding.lifecycleOwner = this
 
 
-                launch {
-                    movieViewModel.popularMovies.collect { movies ->
-                        movieAdapter.addMovies(movies)
-                    }
-                }
-
-
-                launch {
-                    movieViewModel.error.collect { error ->
-                        if (error.isNotEmpty()) {
-                            Snackbar.make(
-                                recyclerView,
-                                error,
-                                Snackbar.LENGTH_LONG
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }
     }
-
-
 
     private fun openMovieDetails(movie: Movie) {
         val intent = Intent(this, DetailsActivity::class.java).apply {
